03dc47c93b4e4a69d4f9b5961f88f9743f8ace21
Sojith Sunny <sojithsunny29034@gmail.com>
2022-09-26 14:55:03 +0530

    toggle switch

--- Projects/toggle switch/script.js
+++ Projects/toggle switch/script.js
@@ -1,3 +1,4 @@
 document.getElementById("toggle").addEventListener("click", function(){
     document.getElementsByTagName('body')[0].classList.toggle("darkmode");
+    document.getElementsByClassName("switch")[0].classList.toggle("on");
 });
